752179d97bb134abd3c0f33d7754e76234206a56
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-01-14 12:10:34 +0200

    Functional test now checks if we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -6,7 +7,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicity_wait(5)
+        self.browser.implicitly_wait(5)
 
     def tearDown(self):
         self.browser.quit()
@@ -19,26 +20,35 @@ class NewVisitorTest(unittest.TestCase):
         # He notices the page title and header mention
         # to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         self.fail('Finish the test!')
 
-# He is invited to enter To-Do item stright away
+        # He is invited to enter To-Do item stright away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-# He types "Buy milk!" into text box
+        # He types "Buy milk!" into text box
+        # When he hits enter the page updates, and now the
+        # page lists: "1. Buy milk! " as an item in a to-do list
+        inputbox.send_keys('Buy milk!!! ')
 
-# When he hits enter the page updates, and now the
-# page lists: "1. Buy milk! " as an item in a to-do list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy milk!!!' for row in rows))
 
-# There is still a test mox inviting him to add another item
-# He enters "Learn to code" and presses enter again
+        # There is still a test mox inviting him to add another item
+        # He enters "Learn to code" and presses enter again
+        self.fail('Finish the test')
 
-# The page updates and now lists both items
+        # The page updates and now lists both items
 
-# He wonders if the site remembers his items and sees
-# that it generates a unique url for him
+        # He wonders if the site remembers his items and sees
+        # that it generates a unique url for him
 
-# He visits that URL and sees that everything is still there
+        # He visits that URL and sees that everything is still there
 
-# Satisfied, he goes back to sleep
+        # Satisfied, he goes back to sleep
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
