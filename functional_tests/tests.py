a4580298be6406d71c2fdfed981061ae48a69c8c
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-01-15 12:24:45 +0200

    New Functional tests for second list and different urls for different lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,10 +33,13 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # He types "Buy milk!" into text box
-        # When he hits enter the page updates, and now the
+        # When he hits enter the page updates,
+        # he is taken to a new URL and now the
         # page lists: "1. Buy milk! " as an item in a to-do list
         inputbox.send_keys('Buy milk!!!')
         inputbox.send_keys(Keys.ENTER)
+        sten_list_url = self.browser.current_url
+        self.assertRegex(sten_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy milk!!!')
 
         # There is still a test mox inviting him to add another item
@@ -49,9 +52,34 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy milk!!!')
         self.check_for_row_in_list_table('2: Learn to code')
 
-        # He wonders if the site remembers his items and sees
-        # that it generates a unique url for him
-        self.fail("finish the test")
-        # He visits that URL and sees that everything is still there
+        # Now a new user, Anna, comes along the site.
+        ## We will make a new browser session to make sure
+        ## that no info of Sten's is coming through some Cookies etc #
 
-        # Satisfied, he goes back to sleep
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Anna visits the home page. There is no sign of Sten's list
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy milk!!!', page_text)
+        self.assertNotIn('to code', page_text)
+
+        # Anna starts a new list entering a new item. She is less intersing
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Write homework')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Anna gets her own unique URL
+        anna_list_url = self.browser.current_url
+        self.assertRegex(anna_list_url, '/lists/.+')
+        self.assertNotEqual(sten_list_url, anna_list_url)
+
+        # Again there is no trace of Sten's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Write homework', page_text)
+        self.assertIn('to code', page_text)
+
+        # Satisfied, they both go back to sleep
