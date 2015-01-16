8506c2815b66593eabb053f7d29bff1e92952095
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-01-17 01:33:08 +0200

    Use Bootstrap to improve layout

--- lists/migrations/0003_auto_20150116_2150.py
+++ lists/migrations/0003_auto_20150116_2150.py
@@ -7,10 +7,19 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('lists', '0003_list'),
+        ('lists', '0002_item_text'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
         migrations.AddField(
             model_name='item',
             name='list',
